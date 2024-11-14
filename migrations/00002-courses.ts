9b18cae5ae5df9f3605dfd017c190c4df1863dd9
Feras Nasr <feras94@protonmail.com>
2024-11-14 13:01:05 +0100

    add positive id constraints to courses and modules tables

--- migrations/00002-courses.ts
+++ migrations/00002-courses.ts
@@ -3,7 +3,11 @@ import { Sql } from 'postgres';
 export async function up(sql: Sql) {
   await sql`
     CREATE TABLE courses (
-      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (
+        start
+        WITH
+          1 increment by 1 minvalue 1
+      ),
       title varchar(255) NOT NULL,
       image_url text,
       description text,
@@ -14,11 +18,13 @@ export async function up(sql: Sql) {
       created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
       updated_at timestamptz DEFAULT CURRENT_TIMESTAMP,
       FOREIGN key (instructor_id) REFERENCES users (id),
-      FOREIGN key (category_id) REFERENCES categories (id)
+      FOREIGN key (category_id) REFERENCES categories (id),
+      CONSTRAINT positive_course_id CHECK (id > 0)
     );
   `;
   await sql` CREATE INDEX idx_category_id ON courses (category_id); `;
 }
+
 export async function down(sql: Sql) {
   await sql` DROP TABLE IF EXISTS courses; `;
 }
--- migrations/00003-modules.ts
+++ migrations/00003-modules.ts
@@ -3,7 +3,11 @@ import { Sql } from 'postgres';
 export async function up(sql: Sql) {
   await sql`
     CREATE TABLE modules (
-      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (
+        start
+        WITH
+          1 increment by 1 minvalue 1
+      ),
       title varchar(255) NOT NULL,
       description text,
       POSITION integer NOT NULL,
@@ -11,7 +15,8 @@ export async function up(sql: Sql) {
       is_published boolean DEFAULT FALSE,
       is_free boolean DEFAULT FALSE,
       created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
-      updated_at timestamptz DEFAULT CURRENT_TIMESTAMP
+      updated_at timestamptz DEFAULT CURRENT_TIMESTAMP CONSTRAINT positive_module_id CHECK (id > 0),
+      CONSTRAINT positive_module_position CHECK (POSITION >= 0)
     );
   `;
   await sql`CREATE INDEX idx_modules_course_id ON modules (course_id);`;
